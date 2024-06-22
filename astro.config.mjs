b12aba178c9c6c0f61c83b8c9d5f1d7b8fb43640
pouliens <pouliens@gmail.com>
2024-06-22 20:55:58 +0100

    trying more fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,7 +13,7 @@ export default defineConfig({
   site: config.site.base_url ? config.site.base_url : "https://pouliens.github.io",
   base: config.site.base_path ? config.site.base_path : "/louie-the-cat/",
   outDir: 'dist', // Output directory
-  publicDir: 'static',
+  // publicDir: 'static',
   trailingSlash: config.site.trailing_slash ? "always" : "never",
   image: {
     service: squooshImageService(),
